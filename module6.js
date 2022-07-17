c39ccc81216505ae1c93afc9a2d1ffc2185bf261
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-07-17 13:18:48 +0300

    add styles

--- module6.js
+++ module6.js
@@ -12,15 +12,45 @@
 // const listItems = list.children;
 // console.log(listItems);
 
-//===================================================
-const message = document.querySelector("#message");
-console.log(message.value); // Default textarea message
+// const text = document.querySelector(".article-text");
+// console.log(text.textContent);
 
-//===================================================
-const activeLink = document.querySelector(".btn.active");
-console.log(activeLink.href); // https://s.codepen.io/about
+// const text = document.querySelector(".article-text");
+// console.log(text.textContent); // text inside p.article-text
 
-//===================================================
-const image = document.querySelector(".image");
-console.log(image.src); // https://placeimg.com/640/480/animals
-image.src = "https://placeimg.com/640/480/tech";
+// const title = document.querySelector(".article-title");
+// // title.textContent = "Welcome to Bahamas!";
+// // title.textContent = "Go home!)";
+// console.log(title.textContent);
+
+// В свойстве classList хранится объект с методами для работы с классами элемента.
+
+// elem.classList.contains(cls) - возвращает true или false в зависимости от того, есть ли у элемента класс cls.
+// elem.classList.add(cls) - добавляет класс cls в список классов элемента.
+// elem.classList.remove(cls) - удаляет класс cls из списка классов элемента.
+// elem.classList.toggle(cls) - если класса cls нет, то добавляет его, если есть, наоборот удаляет.
+// elem.classList.replace(oldClass, newClass) - заменяет существующий класс oldClass на указанный newClass.
+
+const text = document.querySelector("#paragraph");
+
+console.log(text.classList); // ["text", "red", "big", value: "text red big"]
+
+console.log(text.classList.contains("red")); // true
+
+text.classList.remove("big");
+console.log(text.classList); // ["text", "red", value: "text red"]
+
+text.classList.add("new-class");
+console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
+
+// Can add multiple classes
+// text.classList.add("a", "b", "c");
+// console.log(text.classList);
+
+text.classList.toggle("is-hidden"); // will add is-hidden class
+// text.classList.toggle("is-hidden"); // will remove is-hidden class
+console.log(text.classList);
+// classList has a forEach method
+text.classList.forEach((cls) => {
+  console.log(cls); // text, red, new-class
+});
